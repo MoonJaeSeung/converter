4344f1335fee250f77df0b0849c02f1eeb67aa82
MoonJaeSeung <wotmdskfk32@naver.com>
2023-05-11 14:52:36 +0900

    first

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        request.getParameter("data");
+    }
+}
